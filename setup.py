833356705dc77e71010c57bfe360d6689eb61d7d
Alan Collins <alan.collins@bath.edu>
2022-11-30 16:46:19 -0500

    update version, specify python version

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup
 
 setup(
     name="elgato",
-    version="1.0.0",
+    version="1.2.0",
+    python_requires='>=3.6',
     scripts = ['el_gato/el_gato.py'],
     install_requires=[
         "colorama; platform_system == 'Linux'",
